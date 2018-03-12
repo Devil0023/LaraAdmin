8ebe8498ada476678d208b493d76b0b3bac7d933
ZL-PC\Administrator <devil0023@gmail.com>
2018-03-12 16:40:26 +0800

    234

--- LaraAdmin/app/Http/Controllers/LA/CardsController.php
+++ LaraAdmin/app/Http/Controllers/LA/CardsController.php
@@ -219,7 +219,11 @@ class CardsController extends Controller
 			for ($j=0; $j < count($this->listing_cols); $j++) { 
 				$col = $this->listing_cols[$j];
 				if($fields_popup[$col] != null && starts_with($fields_popup[$col]->popup_vals, "@")) {
-					$data->data[$i][$j] = ModuleFields::getFieldValue($fields_popup[$col], $data->data[$i][$j]);
+				    if($col === "img"){
+                        $data->data[$i][$j] = "yes".ModuleFields::getFieldValue($fields_popup[$col], $data->data[$i][$j]);
+                    }else{
+                        $data->data[$i][$j] = ModuleFields::getFieldValue($fields_popup[$col], $data->data[$i][$j]);
+                    }
 				}
 				if($col == $this->view_col) {
 					$data->data[$i][$j] = '<a href="'.url(config('laraadmin.adminRoute') . '/cards/'.$data->data[$i][0]).'">'.$data->data[$i][$j].'</a>';
