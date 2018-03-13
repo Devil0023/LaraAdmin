8adbf37d5be7ca0f023c3deecb6d3322c87acadd
ZL-PC\Administrator <devil0023@gmail.com>
2018-03-13 17:52:17 +0800

    234

--- LaraAdmin/app/Http/admin_routes.php
+++ LaraAdmin/app/Http/admin_routes.php
@@ -85,4 +85,5 @@ Route::group(['as' => $as, 'middleware' => ['auth', 'permission:ADMIN_PANEL']], 
 	//TEST POST
     Route::get("laraadmin.adminRoute". "/testpost/postdata", 'LA\TestpostController@postdata');
     Route::post("laraadmin.adminRoute". "/testpost/postdatapost", 'LA\TestpostController@postdatapost');
+    
 });
