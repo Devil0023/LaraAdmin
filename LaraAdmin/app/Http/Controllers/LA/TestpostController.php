b02ffa6657ee103e14ffc9ab3990d655bac5ffea
ZL-PC\Administrator <devil0023@gmail.com>
2018-03-13 17:51:30 +0800

    1111

---
+++ LaraAdmin/app/Http/Controllers/LA/TestpostController.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Controller genrated using LaraAdmin
+ * Help: http://laraadmin.com
+ */
+
+namespace App\Http\Controllers\LA;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use Auth;
+use DB;
+use Validator;
+use Datatables;
+use Collective\Html\FormFacade as Form;
+use Dwij\Laraadmin\Models\Module;
+use Dwij\Laraadmin\Models\ModuleFields;
+
+use App\Models\Testmodule;
+
+class TestpostController extends Controller
+{
+
+
+    public function __construct() {
+        // Field Access of Listing Columns
+//        if(\Dwij\Laraadmin\Helpers\LAHelper::laravel_ver() == 5.3) {
+//            $this->middleware(function ($request, $next) {
+//                $this->listing_cols = ModuleFields::listingColumnAccessScan('Testmodules', $this->listing_cols);
+//                return $next($request);
+//            });
+//        } else {
+//            $this->listing_cols = ModuleFields::listingColumnAccessScan('Testmodules', $this->listing_cols);
+//        }
+    }
+
+    /**
+     * Display a listing of the Testmodules.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+//        $module = Module::get('Testmodules');
+//
+//        if(Module::hasAccess($module->id)) {
+//            return View('la.testmodules.index', [
+//                'show_actions' => $this->show_action,
+//                'listing_cols' => $this->listing_cols,
+//                'module' => $module
+//            ]);
+//        } else {
+//            return redirect(config('laraadmin.adminRoute')."/");
+//        }
+    }
+
+    public function postdata(){
+        echo "Hello";
+    }
+
+    public function postdatapost(){
+        echo "Hello2";
+    }
+
+}
