94058679253636b6cafdcbbc23532c68b4bbdf55
ZL-PC\Administrator <devil0023@gmail.com>
2018-03-14 09:57:14 +0800

    123123

--- LaraAdmin/resources/views/la/testpost/index.blade.php
+++ LaraAdmin/resources/views/la/testpost/index.blade.php
@@ -1,31 +1,16 @@
+{!! Form::open(['action' => 'LA\TestpostController@postdatapost', 'id' => 'testmodule-add-form']) !!}
 
-<div class="modal fade" id="AddModal" role="dialog" aria-labelledby="myModalLabel">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title" id="myModalLabel">Add Testmodule</h4>
-            </div>
-            {!! Form::open(['action' => 'LA\TestpostController@postdatapost', 'id' => 'testmodule-add-form']) !!}
-            <div class="modal-body">
-                <div class="box-body">
+        {{--@la_form($module)--}}
+        <input type="text" name="name" value="Tom">
+        <input type="text" name="address" value="Shanghai">
 
-                    {{--@la_form($module)--}}
-                    <input type="text" name="name" value="Tom">
-                    <input type="text" name="address" value="Shanghai">
-                    <input type="submit" name="submit" value="submit">
-                    {{--
-                    @la_input($module, 'name')
-                    @la_input($module, 'address')
-                    @la_input($module, 'submit')
-                    --}}
-                </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                {!! Form::submit( 'Submit', ['class'=>'btn btn-success']) !!}
-            </div>
-            {!! Form::close() !!}
-        </div>
-    </div>
-</div>
+        {{--
+        @la_input($module, 'name')
+        @la_input($module, 'address')
+        @la_input($module, 'submit')
+        --}}
+
+
+{!! Form::submit( 'Submit', ['class'=>'btn btn-success']) !!}
+
+{!! Form::close() !!}
